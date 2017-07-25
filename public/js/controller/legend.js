492a6b424a6cb5837463a739582ed7c2a52b98ce
mikemajesty <mikemajesty2011@gmail.com>
2017-07-24 23:53:18 -0300

    add input comparator

--- public/js/controller/legend.js
+++ public/js/controller/legend.js
@@ -2,7 +2,12 @@
   'use strict';
   angular.module('app')
     .controller('Legend', ['$scope', function($scope) {
-      
+       $scope.model = {user1: "", user2: ""};
+
+			 $scope.compare = function () {
+				 console.log("user1",  $scope.model.user1);
+				 console.log("user2",  $scope.model.user2);
+			 };
     }]);
 
 })(window.angular);
