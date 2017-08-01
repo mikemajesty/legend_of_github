28411f89c4b5fb979d9708dc7dc7ced18682201c
mikemajesty <mikemajesty2011@gmail.com>
2017-07-31 22:59:37 -0300

    now is fixing

--- public/js/controller/legend-controller.js
+++ public/js/controller/legend-controller.js
@@ -15,9 +15,9 @@
 
         var findUser1Streak = LegendService.findStreak($scope.model.user1);
 
-        //var findUser2Streak = LegendService.findStreak($scope.model.user2);
+        var findUser2Streak = LegendService.findStreak($scope.model.user2);
 
-        $q.all([findUser1, findUser2, findUser1Streak]).then(function (data) {
+        $q.all([findUser1, findUser1Streak, findUser2, findUser2Streak]).then(function (data) {
 
           var user1Result = data[0];
           console.log("name", user1Result.name);
@@ -26,17 +26,18 @@
           console.log("followers", user1Result.followers);
           console.log("following", user1Result.following);
 
-          /*var user2Result = data[1];
+          var user2Result = data[1];
           console.log("name", user2Result.name);
           console.log("created_at", user2Result.created_at);
           console.log("public_repos", user2Result.public_repos);
           console.log("followers", user2Result.followers);
-          console.log("following", user2Result.following);*/
+          console.log("following", user2Result.following);
 
           var user1Streak = data[2];
-          console.log('current streak', user1Streak.streak || 0);
+          console.log('current streak: '+ $scope.model.user1, user1Streak.streak || 0);
 
-          //var user2Streak = data[3];
+          var user2Streak = data[3];
+           console.log('current streak: '+ $scope.model.user2, user2Streak.streak || 0);
         });
       };
     }]);
