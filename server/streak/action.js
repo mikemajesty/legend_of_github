50e68847d8858383f41f92704b93609e40be3e6d
mikemajesty <mikemajesty2011@gmail.com>
2017-07-31 22:08:15 -0300

    add action to scrap

--- server/streak/action.js
+++ server/streak/action.js
@@ -1,19 +1,10 @@
 const streakService = require("./service");
 
 const getScrapStreak = (req, res, next) => {
-
-	const data = streakService.getStreakBody(req.body.username);
-
-	data.then(function (params) {
-		console.log('0', params[0])
-		console.log('1', params[1])
-		console.log('2', params[2])
-
-		console.log('294', params[294])
-		console.log('295', params[295])
-		console.log('296', params[296])
-
-		res.send(JSON.stringify({ streak: params.length }));
+	streakService.getStreakBody(req.body.username).then(function (params) {
+		res.send(JSON.stringify({
+			streak: params.length
+		}));
 	});
 };
 
