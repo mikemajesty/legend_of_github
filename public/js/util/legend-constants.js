958de1675ddbb17b9e50ee9a2d378335a4a1cd65
mikemajesty <mikemajesty2011@gmail.com>
2017-08-01 22:16:01 -0300

    remove js

--- public/js/util/legend-constants.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-    'use strict';
-
-    angular.module('Legend').constant('LegendConstants', {
-        API: 'https://api.github.com/',
-        STREAK: 'https://github.com/users/'
-    });
-
-})(window.angular);
