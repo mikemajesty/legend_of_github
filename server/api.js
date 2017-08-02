764fffb605ddb1d128326b0b87d207ed3f73448c
mikemajesty <mikemajesty2011@gmail.com>
2017-08-01 22:43:38 -0300

    add repository front end router

--- server/api.js
+++ server/api.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/streak', require('./streak/router'));
-
 router.use('/user', require('./baseInformation/router'));
+router.use('/repository', require('./repository/router'));
 
 router.use('/*', (req, res) => {
   res.status(404).send();
