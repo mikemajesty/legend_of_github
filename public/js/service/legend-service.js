87792a720ccd409ee48c2d601c1a8a71fe34a0cd
mikemajesty <mikemajesty2011@gmail.com>
2017-08-08 21:46:38 -0300

    change js path

--- public/js/service/legend-service.js
+++ public/js/service/legend-service.js
@@ -3,25 +3,25 @@
 
   angular.module('Legend')
 
-  .factory("LegendService", ['$http', function($http) {
+  .factory("LegendService", ['$http', ($http) => {
 
-    var findUser = function(userName) {
-      return $http.post('/user', { username: userName }).then(function(res) {
+    const findUser = (userName) => {
+      return $http.post('/user', { username: userName }).then((res) => {
         return res.data;
       });
     };
 
-    var findStreak = function(userName) {
-      return $http.post('/streak', { username: userName }).then(function(res) {
-        var currentStreak = [];
+    const findStreak = (userName) => {
+      return $http.post('/streak', { username: userName }).then((res) => {
+        let currentStreak = [];
 
-        res.data.filter(function(data) {
-          var date = data.date.split('-');
+        res.data.filter((data) => {
+          const date = data.date.split('-');
           return new Date(date[0], date[1], date[2],0,0,0,0).getTime() < new Date().getTime();
-        }).forEach(function(data, index) {
-          var date = data.date;
-          var currentCommit = data.commit;
-          var lastCommit = 0;
+        }).forEach((data, index) => {
+          const date = data.date;
+          const currentCommit = data.commit;
+          let lastCommit = 0;
 
           if (userName === 'mikemajesty') {
             console.log('currentCommit: ' + currentCommit + ' - lastCommit: ' + lastCommit + ' - date: ' + date);
@@ -44,10 +44,8 @@
       });
     };
 
-    var getRepository = function(userName) {
-      return $http.post('/repository', { username: userName }).then(function(res) {
-        return res.data;
-      });
+    const getRepository = (userName) => {
+      return $http.post('/repository', { username: userName }).then(res => res.data);
     };
 
     return {
