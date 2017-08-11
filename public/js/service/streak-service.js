21ec0e1b5a27d9888dc91a39c7ca4bca8661d276
mikemajesty <mike.lima@tegra.me>
2017-08-11 16:01:56 -0300

    add more features

--- public/js/service/streak-service.js
+++ public/js/service/streak-service.js
@@ -3,37 +3,48 @@
 
   angular.module('Legend')
 
-  .factory("StreakService", ['$http', ($http) => {
+    .factory("StreakService", ['$http', ($http) => {
 
-    const findStreak = (userName) => {
-      return $http.post('/streak', { username: userName }).then((res) => {
-        let currentStreak = [];
-        let lastCommit = 0;
-        res.data.forEach(function(data, index) {
-          const date = data.date;
-          const currentCommit = data.commit;
-
-          if (new Date(data.date.replace('-', '/')).getTime() <= new Date().getTime()) {
-            if (currentCommit > 0 && lastCommit > 0 || index === 0) {
-
-              currentStreak.push({
-                date: date,
-                commit: currentCommit
-              });
-
-            } else {
-              currentStreak = [];
+      const findStreak = (userName) => {
+        return $http.post('/streak', { username: userName }).then((res) => {
+          let currentStreak = [];
+          let lastCommit = 0;
+          res.data.forEach(function(data, index) {
+            const date = data.date;
+            const currentCommit = data.commit;
+            if (userName == 'samfrezza') {
+              console.log('date', date);
+              console.log('currentCommit', currentCommit);
             }
-          }
-          lastCommit = data.commit;
-        });
-        return currentStreak.length;
-      });
-    };
 
-    return {
-      findStreak
-    };
-  }]);
+            if (new Date(data.date.replace('-', '/')).getTime() <= new Date().getTime()) {
+              if (currentCommit > 0 && lastCommit > 0 || index === 0) {
+
+                currentStreak.push({
+                  date: date,
+                  commit: currentCommit
+                });
+
+              } else {
+                if (res.data.length === (index + 1) && currentCommit > 0) {
+                  currentStreak.push({
+                    date: date,
+                    commit: currentCommit
+                  });
+                  return;
+                }
+                currentStreak = [];
+              }
+            }
+            lastCommit = data.commit;
+          });
+          return currentStreak.length;
+        });
+      };
+
+      return {
+        findStreak
+      };
+    }]);
 
 })(window.angular);
