6dbfad6b50a5ccde6f514682b600fb931e7245e5
mikemajesty <mikemajesty2011@gmail.com>
2017-08-10 23:23:21 -0300

    add more information to repositopry scrap

--- public/js/service/repository-service.js
+++ public/js/service/repository-service.js
@@ -6,7 +6,20 @@
   .factory("RepositoryService", ['$http', ($http) => {
 
     const getRepository = (userName) => {
-      return $http.post('/repository', { username: userName }).then(res => res.data);
+      return $http.post('/repository', { username: userName }).then((res) => {
+
+        const sumStarAndFork = _.max(_.map(res.data, (value) => {
+          return (parseInt(value.stars) + parseInt(value.forks));
+        }));
+
+        const bestRepositoty = _.find(res.data, (value) => { return (parseInt(value.stars) + parseInt(value.forks)) == sumStarAndFork;});
+
+        const starts = _.sumBy(res.data, (value) => { return parseInt(value.stars); });
+
+        const forks = _.sumBy(res.data, (value) => { return parseInt(value.forks); });
+
+        return { starts, forks, repositories: res.data, bestRepositoty }
+      });
     };
 
     return {
