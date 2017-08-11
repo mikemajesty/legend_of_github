28f36937cc2bfeffa0808ece4a62e5b778b610b7
mikemajesty <mikemajesty2011@gmail.com>
2017-08-10 21:53:27 -0300

    fix get streak bug

--- public/js/service/streak/streak-service.js
+++ public/js/service/streak/streak-service.js
@@ -8,32 +8,26 @@
     const findStreak = (userName) => {
       return $http.post('/streak', { username: userName }).then((res) => {
         let currentStreak = [];
- 
-        res.data/*.filter((data) => {
-          const date = data.date.split('-');
-          return new Date(date[0], date[1], date[2],0,0,0,0).getTime() < new Date().getTime();
-        })*/.forEach(function (data, index) {
+        let lastCommit = 0;
+        res.data.forEach(function(data, index) {
           const date = data.date;
           const currentCommit = data.commit;
-          let lastCommit = 0;
 
-          if (userName === 'mikemajesty') {
-            //console.log('currentCommit: ' + currentCommit + ' - lastCommit: ' + lastCommit + ' - date: ' + date);
+          if (new Date(data.date.replace('-', '/')).getTime() <= new Date().getTime()) {
+            if (currentCommit > 0 && lastCommit > 0 || index === 0) {
+
+              currentStreak.push({
+                date: date,
+                commit: currentCommit
+              });
+
+            } else {
+              currentStreak = [];
+            }
           }
-          if (currentCommit > 0 && lastCommit > 0 || index === 0) {
-
-            currentStreak.push({
-              date: date,
-              commit: currentCommit
-            });
-
-          } else {
-            currentStreak = [];
-          }
-          console.log('puta vida', data);
           lastCommit = data.commit;
         });
-        return currentStreak;
+        return currentStreak.length;
       });
     };
 
