e046c5c9eaf301fd58056edbc52b6e167d4943d0
mikemajesty <mikemajesty2011@gmail.com>
2017-08-13 22:23:25 -0300

    fix calculate organization bug

--- public/js/service/calculate-avatar-skills.js
+++ public/js/service/calculate-avatar-skills.js
@@ -9,14 +9,14 @@
       const repositories = avatar.repository;
       const streak = avatar.currentStreak;
       const information = avatar.information;
-      const hp = ((information.commits + streak) * information.commitsAvarage) + ((information.fallowers + information.fallowing + information.stars) * information.organizations === 0 ? 1 : information.organizations);
+      const hp = ((information.commits + streak) * information.commitsAvarage) + ((information.fallowers + information.fallowing + information.stars) * (information.organizations === 0 ? 1 : information.organizations));
       const mp = ((information.commits + streak) * information.commitsAvarage);
       const pAtack = ((repositories.forks + repositories.stars + repositories.repositories.full.length) * repositories.repositories.relevants.length) +
         ((information.commits) * (information.commitsAvarage + information.organizations));
-      const pDef = ((repositories.forks + repositories.stars + repositories.repositories.full.length) * repositories.repositories.relevants.length) +
+      const pDef   = ((repositories.forks + information.fallowers + information.fallowing + repositories.stars + repositories.repositories.full.length) * (repositories.repositories.relevants.length === 0 ? 1 : repositories.repositories.relevants.length)) +
         (information.commits * information.commitsAvarage);
-      const cast_speed = (information.commits + ((streak === 0 ? 1 : streak) * information.organizations));
-      const accuracy = ((information.fallowers + information.fallowing) * information.organizations === 0 ? 1 : information.organizations);
+      const cast_speed = (information.commits + ((streak === 0 ? 1 : streak) * (information.organizations === 0 ? 1 : information.organizations)));
+      const accuracy = ((information.fallowers + information.fallowing) * (information.organizations === 0 ? 1 : information.organizations));
       const stamina = (information.commits * information.commitsAvarage);
       const resistence = (information.commits + streak);
 
