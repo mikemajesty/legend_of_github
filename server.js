240da25ed9726b223a32c7fbe76012fe3259e911
mikemajesty <mike.lima@tegra.me>
2017-08-31 08:46:55 -0300

    fix warning

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('serve-favicon');
 const app = express();
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 app.use('/', express.static(__dirname + '/'));
 
