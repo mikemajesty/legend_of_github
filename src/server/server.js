d856cfb5349728c350fca6df9b8b97575e4365b3
mikemajesty <mikemajesty2011@gmail.com>
2017-10-04 23:49:47 -0300

    add favicon

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const port = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
-
-const port = process.env.PORT || 8081;
+app.use(favicon('./static/img/favicon.ico'));
 
 app.listen(port, () => {
   console.log('API listening on port ' + port);
